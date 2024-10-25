531890349516657983cc4355f718cd4af5b9937f
ignlormar <ignlormar@alu.edu.gva.es>
2024-10-25 13:06:19 +0200

    Crear y modificar mutableLists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 
     println(solarSystem.size)
     println("")
@@ -17,6 +17,19 @@ fun main() {
         println(planet)
     }
     println("")
+
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    println("")
+
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
+
 
 
 }
