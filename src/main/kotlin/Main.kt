636c8db73fd390a929adf920f9635a7e8b17cbeb
ignlormar <ignlormar@alu.edu.gva.es>
2024-10-25 13:22:57 +0200

    Crear y modificar mutableMaps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,28 @@
 fun main() {
 
-    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
 
     println(solarSystem.size)
-    solarSystem.add("Pluto")
+    solarSystem["Pluto"] = 5
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
-    println("")
-
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    println(solarSystem["Theia"])
     println("")
 
     solarSystem.remove("Pluto")
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+    println("")
+
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
 
 }
