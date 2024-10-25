53e477057f70af559a22decbace4c29370a19a2b
ignlormar <ignlormar@alu.edu.gva.es>
2024-10-25 13:01:55 +0200

    Crear y modificar lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,22 @@
 fun main() {
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf<String>("Jupiter", "Saturn", "Uranus", "Neptune")
 
-    val solarSystem = rockPlanets + gasPlanets
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 
-    println(solarSystem[0])
-    println(solarSystem[1])
+    println(solarSystem.size)
+    println("")
+
     println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-
-    solarSystem[3] = "Little Earth"
-
+    println(solarSystem.get(3))
     println("")
-    println(solarSystem[3])
 
-    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
-
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
     println("")
-    println(newSolarSystem[8])
+
+    for (planet in solarSystem){
+        println(planet)
+    }
+    println("")
+
+
 }
