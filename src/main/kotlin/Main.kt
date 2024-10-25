6f47a0ab258b7685e29327ec258fb520e967b8d8
ignlormar <ignlormar@alu.edu.gva.es>
2024-10-25 13:13:15 +0200

    Crear y modificar mutableSets

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,19 @@
 fun main() {
 
-    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 
     println(solarSystem.size)
-    println("")
-
-    println(solarSystem[2])
-    println(solarSystem.get(3))
-    println("")
-
-    println(solarSystem.indexOf("Earth"))
-    println(solarSystem.indexOf("Pluto"))
-    println("")
-
-    for (planet in solarSystem){
-        println(planet)
-    }
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
     println("")
 
     solarSystem.add("Pluto")
-    solarSystem.add(3, "Theia")
-    solarSystem[3] = "Future Moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
+    println(solarSystem.size)
     println("")
 
-    solarSystem.removeAt(9)
-    solarSystem.remove("Future Moon")
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
     println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
-
-
 
 }
